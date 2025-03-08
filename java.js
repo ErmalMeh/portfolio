7062a750384d06f4870c51021554fa85f60e60d7
ErmalMeh <ermalmehmeti46@gmail.com>
2025-03-08 01:32:15 +0100

    Update java.js

--- java.js
+++ java.js
@@ -1,5 +1,3 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();  
-    alert("Thank you for your message! I'll get back to you soon.");
-   
+document.querySelector('.scroll-down').addEventListener('click', function() {
+    document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
 });
